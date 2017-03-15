f33f14a807f22f43a1b4b7a52bd2d37c18003df3
h3llowor1d <hao.zhan@oneoaas.com>
2017-03-15 15:57:35 +0800

    udpate setting

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -12,8 +12,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'bkno1',                        # 你的数据库password
+        'USER': 'zabbix',                            # 你的数据库user
+        'PASSWORD': 'zabbix',                        # 你的数据库password
         'HOST': '10.154.33.35',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
